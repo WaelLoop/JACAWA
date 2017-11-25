cd852ec157bed40307bbd08ba95636ef10bf1b95
Artsiom Skliar <artsiom.sk@gmail.com>
2017-11-25 16:26:57 -0500

    updated IDgenerator.py

--- Database/IDgenerator.py
+++ Database/IDgenerator.py
@@ -1,6 +1,10 @@
-'''
-Created on Nov 25, 2017
+#This program will generate IDs for every new entry of a refugee in the database. 
+import cgi
 
-@author: ayoub
-'''
-#its working
+form = cgi.FieldStorage()
+
+dob = form.getvalue("dob")
+fname = form.getvalue("fname")
+lname = form.getvalue("lname")
+
+def ID_generator(bday, fname, lname):
