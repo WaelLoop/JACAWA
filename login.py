1f1e04483be174ab1b9e0902c81ec0b4664d52d3
CPanaritis <christos.panaritis@mail.mcgill.ca>
2017-11-25 16:51:21 -0500

    fixed python

--- login.py
+++ login.py
@@ -3,7 +3,15 @@ print "Content-Type: text/html\n\n"
 form = cgi.FieldStorage()
 user= form.getvalue("username")
 password= form.getvalue("password")
-if user = "Jacawa@mail.mcgill.ca" and password = "abc123":
-    print "success"
+if user == "Jacawa@mail.mcgill.ca" and password == "abc123":
+    print( '<html>')
+    print( '<head></head>')
+    print( '<body>')
+    print( '<h2>SUCCESS</h2>')
+    print( '</body></html>')
 else:
-    print"INCORRECT PASSWORD"
+    print( '<html>')
+    print( '<head></head>')
+    print( '<body>')
+    print( '<h2>INCORRECT LOGIN</h2>')
+    print( '</body></html>')
