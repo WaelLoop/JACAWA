a6b9fe8c16382af480cf550b893c3f34b02a1bb7
CPanaritis <christos.panaritis@mail.mcgill.ca>
2017-11-25 22:58:36 -0500

    fixed the add new refuggee

--- scripts/AddNewEntry.py
+++ scripts/AddNewEntry.py
@@ -1,5 +1,9 @@
-def addNewEntry(iD, firstName, lastName, location ,healthStatus):
-    f = open("refugeedb.csv", "a+")
-    f.write("\n"+ iD + "," + firstName + ","+ lastName + "," + location + "," + healthStatus)
+def addNewEntry(id, dob, firstName, lastName, location ,healthStatus):
+    if int(id[:2]) < 20:
+        filename = '../data/20'+id[:2]+".csv"
+    else:
+        filename = '../data/19'+id[:2]+".csv"
+    f = open(filename, "a+")
+    f.write(id + ", " + dob + ", "+firstName + ", "+ lastName + ", " + location + ", " + healthStatus +"\n")
     f.close()
     return
