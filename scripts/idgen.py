ad9aec4203897e42996e2c8abe4292fce20e7a75
Artsiom Skliar <artsiom.sk@gmail.com>
2017-11-25 21:37:50 -0500

    idgen.py complete

--- scripts/idgen.py
+++ scripts/idgen.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
+'''
+Created on Nov 25, 2017
 
+@author: ayoub
+'''
+
+def IDgenerator(dob, fname, lname):
+    
+    dob = dob.split("/")
+        
+    dob[2] = dob[2][2:]
+    
+    fname = list(fname)
+    lname = list(lname)
+    
+    
+    
+    ID = dob[2] + dob[1] + dob[0] + str(ord(fname[0])-97) + str(ord(fname[1])-97) + str(ord(fname[2])-97) + str(ord(lname[0])-97) + str(ord(lname[1])-97) + str(ord(lname[2])-97)
+        
+    return ID
+    
+    
+print(IDgenerator("02/08/1997", "ayoub", "elhanchi"))
